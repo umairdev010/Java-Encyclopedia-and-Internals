4504f27a0e9f71401afa7297a4096b30cd6a4604
Umair Tariq <umairdev010@gmail.com>
2026-01-26 16:43:26 +0500

    adding new topic on monday evening

--- src/Overloading.java
+++ src/Overloading.java
@@ -8,6 +8,36 @@ public class Overloading {
         // In method overloading, method must have different parameters type.
         // If different return type, it is not alone.
 
+        Square newSquare = new Square(6);
+        newSquare.area();   // its name is area
+        newSquare.area(3); // its name is also area.
+        // but we can call both one without arguments. And one with arguments
+
+
+        // CONSTRUCTOR OVERLOADING
+        // we can also overload constructor method
+
+        Square newSquare1 = new Square(); // here we make object of Square without passing any length to constructor
 
     }
 }
+ class Square{
+    int length;
+
+    Square(){
+        System.out.println("NEW SQUARE CREATED WITHOUT PARAMENTERS");
+    }
+
+    Square(int length){
+        this.length = length;
+    }
+
+    void area(){
+        System.out.println("Area of the square is " + length*length);
+    }
+
+    void area(int length){
+        System.out.println("Area of the square is " + length*length);
+    }
+
+ }
