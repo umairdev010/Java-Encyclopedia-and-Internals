0066128d4956aba0e2ef80ac3ae6f8a59986242e
Umair Tariq <umairdev010@gmail.com>
2026-01-26 16:31:27 +0500

    adding new topic on monday evening

---
+++ src/Overloading.java
@@ -0,0 +1,13 @@
+public class Overloading {
+    static void main(String[] args) {
+
+        // METHOD OVERLOADING
+        // In java class we can define same functions/methods with the same name as long as their parameters are different. this is called overloading.
+        // And the process is called method overloading
+        // method overloading is one of the way that tells that java supports polymorphism.
+        // In method overloading, method must have different parameters type.
+        // If different return type, it is not alone.
+
+
+    }
+}
