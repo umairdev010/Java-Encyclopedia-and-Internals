05e37630ff57052fac758c913f7e5294d6cc23d0
Umair Tariq <umairdev010@gmail.com>
2026-01-27 16:20:54 +0500

    adding new topic on tuseday

---
+++ src/AccessControl.java
@@ -0,0 +1,30 @@
+public class AccessControl {
+    static void main(String[] args) {
+
+        // INTRODUCTION TO ACCESS CONTROL
+        // Encapsulation gives us another control "access control" which through which we can control which part of class can be access by other class
+        // By using Access control we can prevent misuse
+        // How we can access a variable , method and class is determined by its access modifier.
+        // There are Four 04 types of access modifier
+        // 1. Public
+        // 2. private
+        // 3. Protected
+        // 4. Default
+
+        // 1. PUBLIC
+        // If a member is public then it can be access by any other part of any code.
+
+        // 2. PRIVATE
+        // If a member is private then it can only access by members of its class. anyone from outside cannot access it
+
+        // 3.PROTECTED
+        // It is only used in case of inheritance. Member is used by its children only.
+
+        // 4. DEFAULT
+        // If we don't specify any access modifier then its default.
+        // In case of default members of class is accessed by all other classes but in the same package
+
+
+
+    }
+}
