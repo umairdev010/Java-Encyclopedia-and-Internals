48c9f2e184855ed69ed99c185adb46f6d4349412
Umair Tariq <umairdev010@gmail.com>
2026-01-28 17:48:27 +0500

    adding new topic on wensday evening

---
+++ src/AbstractClass.java
@@ -0,0 +1,10 @@
+public class AbstractClass {
+    static void main(String[] args) {
+
+        // ABSTRACT CLASS
+        // It is a class in java which cannot be initiated. Means we cannot make its objects.
+        // But we used it as a blueprint / template for building other classes.
+        //
+
+    }
+}
