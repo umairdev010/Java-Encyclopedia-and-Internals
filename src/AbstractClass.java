0625347293e735764c61531079351394ad0e3f9c
Umair Tariq <umairdev010@gmail.com>
2026-01-29 03:02:56 +0500

    complete the topic of Abstract class on thursday morning

--- src/AbstractClass.java
+++ src/AbstractClass.java
@@ -10,7 +10,10 @@ public class AbstractClass {
 
 
         // FINAL KEYWORD
-        //
+        // There is 03 uses of final keyword.
+        // 1. As we see previous we use final keyword with variables to make them constant (value cannot change once assigned)
+        // 2. We can also use it to prevent methods from override.
+        // 3. We can also use the final keyword in front of class. To prevent class from being inherited.
 
     }
 }
@@ -18,6 +21,9 @@ public class AbstractClass {
 
 abstract class Shapes {
     abstract void shape(); //As we make this method absract every subclass of shapes must implement the area method
+    final void intro(){
+        System.out.println("this method is for Shapes only");
+    }
 }
 
 class Rectangle extends Shapes{
@@ -28,6 +34,8 @@ class Rectangle extends Shapes{
         len = l;
         width = w;
     }
+
+    // void intro(){} we cannot override it.
 
     @Override
     void shape() { // it is important for us to design this method otherwise give error.
