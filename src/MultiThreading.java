5d3e33e0928ca7c6a9d64066136a902628eb0b2d
Umair Tariq <umairdev010@gmail.com>
2026-01-31 17:00:23 +0500

    adding new topic on saturday

---
+++ src/MultiThreading.java
@@ -0,0 +1,10 @@
+public class MultiThreading {
+    static void main(String[] args) {
+
+        // MULTITHREADING
+        // Java provides built in multithreading.
+        // Multithreading means program contains two and more parts which running concurrently.
+        //
+
+    }
+}
