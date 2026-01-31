77195f7786988bb75fb2d7c1069591ef5c9ee409
Umair Tariq <umairdev010@gmail.com>
2026-01-31 17:03:23 +0500

    adding some changes in topic on saturday

--- src/MultiThreading.java
+++ src/MultiThreading.java
@@ -4,6 +4,11 @@ public class MultiThreading {
         // MULTITHREADING
         // Java provides built in multithreading.
         // Multithreading means program contains two and more parts which running concurrently.
+        // Each part which is running concurrently is known as thread.
+        // And each part has its own execution.
+        // Multithreading is specialized as multitasking.
+        // There are two types od multitasking.
+        // 1. Process based 2. Thread based
         //
 
     }
