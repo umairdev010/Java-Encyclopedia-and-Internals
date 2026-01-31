28d9482b16510ab99265b66ad63df9d87be22b8f
Umair Tariq <umairdev010@gmail.com>
2026-01-31 17:07:26 +0500

    adding some changes in topic on saturday

--- src/MultiThreading.java
+++ src/MultiThreading.java
@@ -9,6 +9,11 @@ public class MultiThreading {
         // Multithreading is specialized as multitasking.
         // There are two types od multitasking.
         // 1. Process based 2. Thread based
+        // A process is in generally the program which running.
+        // so process based multitasking allow your computer to run two and more program at a time.
+        // In process based multitasking program is the smallest unit of code dispatch by scheduler.
+
+        // In Thread based multitasking thread is the smallest unit of dispatched code.
         //
 
     }
