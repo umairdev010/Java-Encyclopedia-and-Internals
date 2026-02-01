15bf7cb69d3230ca12eb7224ef8bc67a90be1d0e
Umair Tariq <umairdev010@gmail.com>
2026-02-01 16:26:27 +0500

    adding new topic on sunday

---
+++ src/LearningJDBC.java
@@ -0,0 +1,19 @@
+public class LearningJDBC {
+    static void main(String[] args) {
+
+        // JAVA DATABASE CONNECTIVITY
+
+        // There are 7 steps in jdbc
+        // 1. import connector
+        // 2. load driver
+        // 3. register driver
+        // 4. create connection
+        // 5. create statement
+        // 6. execute statement
+        // 7. close connection
+
+
+
+
+    }
+}
