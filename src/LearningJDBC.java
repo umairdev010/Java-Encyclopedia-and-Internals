cde9b056c256389a4f4904b0217554b79e2f1e79
Umair Tariq <umairdev010@gmail.com>
2026-02-01 16:41:50 +0500

    adding changes in topic on sunday

--- src/LearningJDBC.java
+++ src/LearningJDBC.java
@@ -1,3 +1,5 @@
+import java.sql.*;    // here we take first step
+
 public class LearningJDBC {
     static void main(String[] args) {
 
@@ -11,8 +13,21 @@ public class LearningJDBC {
         // 5. create statement
         // 6. execute statement
         // 7. close connection
+        try {
+
+            Class.forName("org.postgresql.Driver");  // here we load driver in the file this is 2nd step which is optional but as safety
+
+            String url = "jdbc:postgresql://localhost:5432/testing";
+            String user = "postgre";
+            String password = "00987";
+
+            Connection conn = DriverManager.getConnection(url,user,password);  // here we make connection step 4
 
 
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
 
 
     }
